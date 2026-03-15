ea73575edd0d00383dbbe4417c9be8b6bbf3f946
Dino-anjai <25082010004@student.upnjatim.ac.id>
2026-03-15 15:14:03 +0700

    Add files via upload

--- TokoSaya/src/tokosaya/TokoSaya3.java
+++ TokoSaya/src/tokosaya/TokoSaya3.java
@@ -5,7 +5,9 @@ public class TokoSaya3 {
     public static void main (String[]args) {
         Scanner sc = new Scanner(System.in);
         cGoods good = new cGoods (111,"Rice",18000,100);
-        cPembeli pembeli = new cPembeli();
+        ArrayList<cPembeli> daftarPembeli = new ArrayList<>();
+        int counterID = 1;
+        cPembeli pembeli1 = new cPembeli ();
         cSales sale = new cSales();
         int choice = 0;
         do{
@@ -15,12 +17,13 @@ public class TokoSaya3 {
             choice=sc.nextInt();
             switch(choice){
                 case 1 :
-                    System.out.println(" Goods Menu ");
+                    System.out.println("------ Goods Menu ---------");
                     System.out.println("  1. Add");
                     System.out.println("  2. Read");
                     System.out.println("  3. Update");
                     System.out.println("  4. Delete");
                     System.out.println("  5. Back");
+                    System.out.println("---------------------------");
                     System.out.print(" Choose =");
                     int choice2=sc.nextInt();
                     switch(choice2){
@@ -54,54 +57,107 @@ public class TokoSaya3 {
                     }
                     break;
                 case 2:
-                    int counterID = 1;
-                    System.out.println(" Buyer Menu ");
+                    System.out.println(" ------ Buyer Menu --------");
                     System.out.println("  1. Add");
                     System.out.println("  2. Read");
                     System.out.println("  3. Update");
                     System.out.println("  4. Delete");
                     System.out.println("  5. Back");
+                    System.out.println("---------------------------");
                     System.out.print(" Choose =");
                     int choice3=sc.nextInt();
                     switch(choice3){
-                        case 1 :
+                         case 1 :
                             // add pembeli
                             System.out.print("Masukkan Nama: ");
                             String n = sc.next();
-                            System.out.println("Masukkan Alamat: ");
+                            System.out.print("Masukkan Alamat: ");
                             String a = sc.next();
-                            Int i = Int.format("%03", counterID++);
+                            int i = counterID++;
                             System.out.println("User berhasil ditambahkan");
-                            System.out.println("ID User: " + i);
-                            pembeli = new cPembeli (n,a,i);
-  
+                            System.out.println("Data anda : " + "\n ID:" + i + "\n Nama : " +n + "\n Alamat:" +a);
+                            cPembeli pembelibaru = new cPembeli (i,n,a);
+                            daftarPembeli.add(pembelibaru);
                             break;
                         case 2 :
-                            
-                            
+                            // Read
+                            if(daftarPembeli.isEmpty()){
+                                    System.out.println("User tidak ditemukan!");
+                            }
+                            else
+                                for(cPembeli p : daftarPembeli){
+                                        System.out.println(p.ToString());
+                                }
                             break;
+                        case 3 :
+                            // update
+                            System.out.print("Masukkan ID Pembeli: ");
+                            int idUpdate = sc.nextInt();
+                            for(cPembeli p : daftarPembeli){
+                            if(p.getsId()== idUpdate){
+                             System.out.print("Alamat baru : ");
+                             String nw = sc.next();
+                             p.setAddres(nw);
+                            System.out.println("Alamat berhasil diubah");
+                                 }
+                            }
+                            break;
+                        case 4 :
+                           System.out.print("Masukkan ID Pembeli yang akan dihapus: ");
+                           int idDelete = sc.nextInt();
+                           for(cPembeli p : daftarPembeli){
+                           if(p.getsId() == idDelete){
+                           daftarPembeli.remove(p);
+                           System.out.println("Pembeli berhasil dihapus");    
+                                }
+                           }
+                           break;
+                        case 5 :
+                           System.out.println("Back to main");
                     }
-                    
-                    
-                    
-                case 3 :
-                    System.out.println(" Sales Menu ");
-                    //idelnya ada CRUD (create, read, update, delete)
-                    System.out.println("Buyer = ");
-                    String b=sc.next();
-                    System.out.print("Goods = ");
-                    String g=sc.next();
-                    if(g.equalsIgnoreCase(good.getName())){
-                      System.out.println("Quantity");
-                      int q = sc.nextInt();
-                      sale.addSales(q, good, pembeli, q);
-                      sale.printSales();  
-                    }
-                    else System.out.println("Goods not found");
+                  break;
+                case 3:
+                System.out.println("------ Sales Menu ------");
+                if(daftarPembeli.isEmpty()){
+                    System.out.println("Belum ada pembeli, silakan tambah di menu Buyer");
                     break;
+                }
+                // pilih pembeli
+                System.out.print("Masukkan ID Pembeli: ");
+                int idBuyer = sc.nextInt();
+                cPembeli pembeliDipilih = null;
+                for(cPembeli p : daftarPembeli){
+                if(p.getsId() == idBuyer){
+                pembeliDipilih = p;
+                    break;
+                }
+                }
+                if(pembeliDipilih == null){
+                System.out.println("Pembeli tidak ditemukan!");
+                break;
+                }
+
+                // tampilkan barang
+                System.out.println("Goods = " + good.getName());
+                System.out.print("Masukkan nama barang: ");
+                String g = sc.next();
+
+                if(g.equalsIgnoreCase(good.getName())){
+                System.out.print("Quantity: ");
+                int q = sc.nextInt();
+
+                sale.addSales(q, good, pembeliDipilih, q);
+                sale.printSales();
+                }
+                else{
+                System.out.println("Barang tidak ditemukan!");
+                }
+            break;
+                
                 case 4:
                     System.out.println("Thank You...");
-            }
-        }while (choice!=4);
+                }
+            }while (choice!=4);
     }
 }
+
